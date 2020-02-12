d8311e9b1a2e768d9926856076ad7109f4bedeb9
Reese <reesericci@gmail.com>
2020-02-12 07:14:58 -0600

    Update material-awesome.sh

--- material-awesome.sh
+++ material-awesome.sh
@@ -1,5 +1,5 @@
 sudo pacman -Sy awesome rofi lxappearance yay pkg-config make fakeroot --noconfirm
-yay -Sy i3lock-fancy
+yay -Sy i3lock-fancy --noconfirm
 git clone https://github.com/tryone144/compton.git
 cd compton
 make
