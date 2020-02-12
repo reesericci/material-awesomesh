d674eaf1e00a67f1ee823e31a25fc46404d69cbd
Reese <reesericci@gmail.com>
2020-02-12 07:09:58 -0600

    Create material-awesome.sh

---
+++ material-awesome.sh
@@ -0,0 +1,6 @@
+sudo pacman -Sy awesome rofi lxappearance yay pkg-config make fakeroot --noconfirm
+yay -Sy i3lock-fancy
+git clone https://github.com/tryone144/compton.git
+cd compton
+make
+make install
