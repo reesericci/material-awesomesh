2571a7bbf837f7b1d099860520a94ca0dd3adf06
Reese <reesericci@gmail.com>
2020-02-14 08:42:35 -0600

    Update material-awesome.sh

--- material-awesome.sh
+++ material-awesome.sh
@@ -4,5 +4,6 @@ git clone https://github.com/tryone144/compton.git
 cd compton
 make
 make install
-git clone https://github.com/HikariKnight/material-awesome.git ~/.config/awesome
+rm ~/.config/awesome/*
+git clone https://github.com/reesericci/material-awesome.git ~/.config/awesome
 echo Log out and log back in under the awesome window manager to view the material-awesome config!
