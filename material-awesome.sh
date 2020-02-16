1f5dbd7b47d5803831627382b8e6ad4cfda447d2
Reese <reesericci@gmail.com>
2020-02-16 12:06:07 -0600

    Update material-awesome.sh

--- material-awesome.sh
+++ material-awesome.sh
@@ -1,9 +1,10 @@
 sudo pacman -Sy awesome rofi flameshot xfce4-power-manager lxappearance blueberry yay pkg-config make fakeroot xorg-xbacklight networkmanager network-manager-applet libx11 libconfig libxcomposite libxdamage libxfixes libxext libxrender libxrandr libdrm asciidoc --noconfirm
 yay -Sy i3lock-fancy ttf-roboto xclip  fluxgui --noconfirm
 git clone https://github.com/tryone144/compton.git
+sudo rm -rf compton
 cd compton
 make
 make install
-rm -rf ~/.config/awesome/*
+sudo rm -rf ~/.config/awesome
 git clone https://github.com/reesericci/material-awesome.git ~/.config/awesome
 echo Log out and log back in under the awesome window manager to view the material-awesome config!
