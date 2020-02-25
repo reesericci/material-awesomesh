61e0824878f4b71b45b84275e780390ede2264bb
Reese <reesericci@gmail.com>
2020-02-25 06:18:32 -0600

    Update material-awesome.sh

--- material-awesome.sh
+++ material-awesome.sh
@@ -1,10 +1,5 @@
 sudo pacman -Sy awesome rofi flameshot xfce4-power-manager lxappearance blueberry yay pkg-config make fakeroot xorg-xbacklight networkmanager network-manager-applet libx11 libconfig libxcomposite libxdamage libxfixes libxext libxrender libxrandr libdrm asciidoc --noconfirm
-yay -Sy i3lock-fancy ttf-roboto xclip  fluxgui --noconfirm
-sudo rm -rf compton
-git clone https://github.com/tryone144/compton.git
-cd compton
-make
-sudo make install
+yay -Sy i3lock-fancy ttf-roboto xclip  fluxgui compton-tryone-git --noconfirm
 sudo rm -rf ~/.config/awesome
 git clone https://github.com/reesericci/material-awesome.git ~/.config/awesome
 cp ./theme/bashrc ~/.bashrc
